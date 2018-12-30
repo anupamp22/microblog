5911b3439987089e1ed8e7c3995c71fa6c4d012a
anupam.p22 <anupam.p22@gmail.com>
2018-12-29 21:44:30 -0800

    adding notifications

--- app/models.py
+++ app/models.py
@@ -7,6 +7,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from app import db, login
 from app.search import add_to_index, remove_from_index, query_index
+import json
+from time import time
 
 
 followers = db.Table(
@@ -77,6 +79,9 @@ class User(UserMixin, db.Model):
         backref='recipient', lazy='dynamic')
     last_message_read_time=db.Column(db.DateTime)
 
+    notifications = db.relationship('Notification', backref='user',
+                                    lazy='dynamic')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -130,6 +135,11 @@ class User(UserMixin, db.Model):
         return Message.query.filter_by(recipient=self).filter(
             Message.timestamp>last_read_time).count()
 
+    def add_notification(self,name, data):
+        self.notifications.filter_by(name=name).delete()
+        n=Notification(name=name,payload_json=json.dumps(data),user=self)
+        db.session.add(n)
+        return n
 
 @login.user_loader
 def load_user(id):
@@ -158,3 +168,14 @@ class Message(db.Model):
 
     def __repr__(self):
         return '<Message {}>'.format(self.body)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
